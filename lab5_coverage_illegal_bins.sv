0576c99bb5f5d78ad8b93eaab818d1c187070593
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2025-12-05 16:43:09 +0530

    Create lab5_coverage_illegal_bins.sv

---
+++ lab5_coverage_illegal_bins.sv
@@ -0,0 +1,29 @@
+module eg;
+  bit [3:0] a;
+  
+  covergroup cg;
+    option.per_instance = 1;
+    /*A1: coverpoint a {
+      option.auto_bin_max = 16;
+      ignore_bins b2 = {2};
+      ignore_bins b3 = {3};
+      ignore_bins b4 = {4};
+      ignore_bins b5 = {5};
+      bins b[] = {[6:$],1};
+    }*/
+    A2: coverpoint a{
+      option.auto_bin_max = 8;
+      illegal_bins b1[] = {[8:11]};
+      bins b2[] = {[$:$]};
+    }
+  endgroup
+  
+  initial begin
+    cg c1 = new();
+    repeat(200) begin
+      a = $urandom;
+      c1.sample();
+    end
+  end
+  
+endmodule
